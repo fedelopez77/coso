a6a5c39acc70ab5ace4a3a33f5847fbc7484ec78
Federico Lopez <fedelopez77@gmail.com>
2018-07-17 17:58:46 +0200

    Add export jsonl

--- coso/utils.py
+++ coso/utils.py
@@ -3,6 +3,12 @@ import json
 BUFFER_SIZE = 64 * (1024 ** 2)
 
 
-def get_jsonl_data(path):
+def load_jsonl(path):
     with open(path, "r", buffering=BUFFER_SIZE) as f:
         return [json.loads(line) for line in f]
+
+
+def export_jsonl(path, data):
+    with open(path, "w", buffering=BUFFER_SIZE) as fp:
+        for res in data:
+            fp.write(json.dumps(res) + "\n")
